0a681b76b6763eaeb2b20932d618c1560d9affe0
Michał Rokita <mrokita@mrokita.pl>
2020-03-25 04:01:48 +0100

    Remove sqlite args

--- api/app/database.py
+++ api/app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from settings import DATABASE_URL
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
